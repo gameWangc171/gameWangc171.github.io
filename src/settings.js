a56d4641bd63576505d7b521dda0127e22d784c8
wangc <aaa1914430028@163.com>
2024-05-19 09:33:26 +0800

    init:初次提交

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","shadow"],collisionMatrix:[[true],[false,false],[false,false,false],[false,false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"",jsList:[]};
