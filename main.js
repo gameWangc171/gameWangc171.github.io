47ee8b7f7cb2328d09a63c4f0d7fd600f2df30a9
wangc <aaa1914430028@163.com>
2024-06-02 09:16:38 +0800

    feat:项目构建

--- main.js
+++ main.js
@@ -1,144 +1,82 @@
-window.boot = function () {
-    var settings = window._CCSettings;
-    window._CCSettings = undefined;
-    var onProgress = null;
-    
-    var RESOURCES = cc.AssetManager.BuiltinBundleName.RESOURCES;
-    var INTERNAL = cc.AssetManager.BuiltinBundleName.INTERNAL;
-    var MAIN = cc.AssetManager.BuiltinBundleName.MAIN;
-    function setLoadingDisplay () {
-        // Loading splash scene
-        var splash = document.getElementById('splash');
-        var progressBar = splash.querySelector('.progress-bar span');
-        onProgress = function (finish, total) {
-            var percent = 100 * finish / total;
-            if (progressBar) {
-                progressBar.style.width = percent.toFixed(2) + '%';
-            }
-        };
-        splash.style.display = 'block';
-        progressBar.style.width = '0%';
+(function () {
 
-        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
-            splash.style.display = 'none';
+    function boot () {
+        window.__quick_compile_project__.destPath = {tempScriptsPath};
+
+        cc.assetManager.init({
+            importBase: {importPath},
+            nativeBase: {nativePath}
         });
-    }
 
-    var onStart = function () {
+        var onStart = function () {
+            cc.view.resizeWithBrowserSize(true);
 
-        cc.view.enableRetina(true);
-        cc.view.resizeWithBrowserSize(true);
+            // init assets
 
-        if (cc.sys.isBrowser) {
-            setLoadingDisplay();
-        }
-
-        if (cc.sys.isMobile) {
-            if (settings.orientation === 'landscape') {
-                cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
-            }
-            else if (settings.orientation === 'portrait') {
-                cc.view.setOrientation(cc.macro.ORIENTATION_PORTRAIT);
-            }
-            cc.view.enableAutoFullScreen([
-                cc.sys.BROWSER_TYPE_BAIDU,
-                cc.sys.BROWSER_TYPE_BAIDU_APP,
-                cc.sys.BROWSER_TYPE_WECHAT,
-                cc.sys.BROWSER_TYPE_MOBILE_QQ,
-                cc.sys.BROWSER_TYPE_MIUI,
-                cc.sys.BROWSER_TYPE_HUAWEI,
-                cc.sys.BROWSER_TYPE_UC,
-            ].indexOf(cc.sys.browserType) < 0);
-        }
-
-        // Limit downloading max concurrent task to 2,
-        // more tasks simultaneously may cause performance draw back on some android system / browsers.
-        // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
-        if (cc.sys.isBrowser && cc.sys.os === cc.sys.OS_ANDROID) {
-            cc.assetManager.downloader.maxConcurrency = 2;
-            cc.assetManager.downloader.maxRequestsPerFrame = 2;
-        }
-
-        var launchScene = settings.launchScene;
-        var bundle = cc.assetManager.bundles.find(function (b) {
-            return b.getSceneInfo(launchScene);
-        });
-        
-        bundle.loadScene(launchScene, null, onProgress,
-            function (err, scene) {
-                if (!err) {
-                    cc.director.runSceneImmediate(scene);
-                    if (cc.sys.isBrowser) {
-                        // show canvas
-                        var canvas = document.getElementById('GameCanvas');
-                        canvas.style.visibility = '';
-                        var div = document.getElementById('GameDiv');
-                        if (div) {
-                            div.style.backgroundImage = '';
-                        }
-                        console.log('Success to load scene: ' + launchScene);
+            // load stashed scene, unlike the standard loading process, here we do some hack to reduce the engine size.
+            cc.assetManager.loadAny({url: 'preview-scene.json', __isNative__: false }, null,
+                function (err, sceneAsset) {
+                    if (err) {
+                        cc.error(err.message, err.stack);
+                        return;
                     }
+                    var scene = sceneAsset.scene;
+                    scene._name = sceneAsset._name;
+                    // HACK: Change key to uuid from url
+                    cc.assetManager.dependUtil._depends.add(scene._id, cc.assetManager.dependUtil._depends.get('preview-scene.json'));
+                    // scene._id = ??;   stashed scene dont have uuid...
+                    cc.director.runSceneImmediate(scene, function () {
+                        // play game
+                        cc.game.resume();
+                    });
                 }
+            );
+
+            // purge
+            //noinspection JSUndeclaredVariable
+            _CCSettings = undefined;
+        };
+
+        var option = {
+            debugMode: cc.debug.DebugMode.INFO,
+            showFPS: true,
+            frameRate: 60,
+            groupList: _CCSettings.groupList,
+            collisionMatrix: _CCSettings.collisionMatrix,
+        };
+
+        var RESOURCES = cc.AssetManager.BuiltinBundleName.RESOURCES;
+        var INTERNAL = cc.AssetManager.BuiltinBundleName.INTERNAL;
+        var MAIN = cc.AssetManager.BuiltinBundleName.MAIN;
+        var bundleRoot = [INTERNAL];
+        _CCSettings.hasResourcesBundle && bundleRoot.push(RESOURCES);
+
+        var count = 0;
+        function cb (err) {
+            if (err) return console.error(err);
+            count++;
+            if (count === bundleRoot.length + 1) {
+                cc.assetManager.loadBundle(MAIN, function (err) {
+                    if (!err) cc.game.run(option, onStart);
+                });
             }
-        );
-
-    };
-
-    var option = {
-        id: 'GameCanvas',
-        debugMode: settings.debug ? cc.debug.DebugMode.INFO : cc.debug.DebugMode.ERROR,
-        showFPS: settings.debug,
-        frameRate: 60,
-        groupList: settings.groupList,
-        collisionMatrix: settings.collisionMatrix,
-    };
-
-    cc.assetManager.init({ 
-        bundleVers: settings.bundleVers,
-        remoteBundles: settings.remoteBundles,
-        server: settings.server
-    });
-    
-    var bundleRoot = [INTERNAL];
-    settings.hasResourcesBundle && bundleRoot.push(RESOURCES);
-
-    var count = 0;
-    function cb (err) {
-        if (err) return console.error(err.message, err.stack);
-        count++;
-        if (count === bundleRoot.length + 1) {
-            cc.assetManager.loadBundle(MAIN, function (err) {
-                if (!err) cc.game.run(option, onStart);
-            });
         }
-    }
+        
+        cc.assetManager.loadScript(_CCSettings.jsList.map(function (x) { return {scriptPath} + x; }), cb);
 
-    cc.assetManager.loadScript(settings.jsList.map(function (x) { return 'src/' + x;}), cb);
-
-    for (var i = 0; i < bundleRoot.length; i++) {
-        cc.assetManager.loadBundle(bundleRoot[i], cb);
-    }
-};
-
-if (window.jsb) {
-    var isRuntime = (typeof loadRuntime === 'function');
-    if (isRuntime) {
-        require('src/settings.js');
-        require('src/cocos2d-runtime.js');
-        if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-            require('src/physics.js');
+        for (var i = 0; i < bundleRoot.length; i++) {
+            cc.assetManager.loadBundle(bundleRoot[i], cb);
         }
-        require('jsb-adapter/engine/index.js');
-    }
-    else {
-        require('src/settings.js');
-        require('src/cocos2d-jsb.js');
-        if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-            require('src/physics.js');
-        }
-        require('jsb-adapter/jsb-engine.js');
+        
     }
 
-    cc.macro.CLEANUP_IMAGE_CACHE = true;
-    window.boot();
-}
+    require({tempScriptsPath} + '__quick_compile__.js');
+    require('src/simulator-config.js');
+    require('src/settings.js');
+    require('src/cocos2d-jsb.js');
+    require('jsb-adapter/jsb-engine.js');
+    require('src/asset-record-pipe.js');
+
+    boot();
+
+})();
