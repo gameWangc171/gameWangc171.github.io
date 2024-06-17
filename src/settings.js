00cbc4cfc991038e92832aa1b9977affdd36bcb6
wangc <aaa1914430028@163.com>
2024-06-17 22:29:24 +0800

    feat:项目构建

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,35 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","shadow"],collisionMatrix:[[true],[false,false],[false,false,false],[false,false,false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"",jsList:[]};
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "shadow"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/Main.fire",
+    orientation: "",
+    debug: true,
+    jsList: []
+};
